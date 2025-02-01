907446b1bbff17363be4b025861baec5cbe176ea
shankkarpal46 <shankkarpal46@gmail.com>
2025-02-01 13:53:08 +0530

    grid and javascript varaible declaration

---
+++ JS/externalJS/script1.js
@@ -0,0 +1,23 @@
+// var a = 12;
+
+// var a = 19;
+
+// let a = 12;
+
+//     a = 19;
+
+// const a = 12;
+
+//     a = 19;
+
+// document.write("The addition is of 12 and 13:",12+13,"<br>");
+// document.write("The subtration is of 12 and 13:",12-13,"<br>");
+// document.write("The division is of 12 and 13:",12/13,"<br>");
+// document.write("The multiplication is of 12 and 13:",12*13,"<br>");
+
+let p = 67;
+let q = 12;
+
+document.write(`Addition of ${p} and ${q} is ${p+q} <br>`)
+document.write(`Power of ${p} to ${q} is ${p**q} <br>`)
+
